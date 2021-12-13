cf83209a584af252372f28714c2b0f6ae221b149
caro.deman20 <caro.deman20@triana.salesianos.edu>
2021-12-13 10:13:25 +0100

    inicio del proyecto

---
+++ e01ManuelCaro/src/main/java/com/e01ManuelCaro/e01ManuelCaro/E01ManuelCaroApplication.java
@@ -0,0 +1,13 @@
+package com.e01ManuelCaro.e01ManuelCaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E01ManuelCaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E01ManuelCaroApplication.class, args);
+	}
+
+}
---
+++ e01ManuelCaro/src/test/java/com/e01ManuelCaro/e01ManuelCaro/E01ManuelCaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e01ManuelCaro.e01ManuelCaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E01ManuelCaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
